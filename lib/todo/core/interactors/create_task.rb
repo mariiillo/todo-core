e288b57f9ea4e364b74f030a22c5d13f5e317fe2
Mario Viapiano <mario@shipcloud.io>
2020-11-18 01:11:23 +0100

    add repository to create_task

--- lib/todo/core/interactors/create_task.rb
+++ lib/todo/core/interactors/create_task.rb
@@ -4,13 +4,23 @@ module Todo
   module Core
     module Interactors
       class CreateTask
+        def initialize(repository: Todo::Core::Repositories::TaskRepository.new)
+          @repository = repository
+        end
+
         def call(name:)
           if name.nil?
             Result.new({ error_message: 'Invalid name' })
           else
+            task = Todo::Core::Entities::Task.new(name: name)
+            repository.save(task)
             Result.new({ name: name, created_at: Time.now })
           end
         end
+
+        private
+
+        attr_reader :repository
       end
 
       Result = Struct.new(:payload) do
--- spec/todo/interactors/create_task_spec.rb
+++ spec/todo/interactors/create_task_spec.rb
@@ -3,14 +3,25 @@
 RSpec.describe Todo::Core::Interactors::CreateTask do
   context 'with valid attributes' do
     it 'returns a sucessfull output' do
-      interactor = described_class.new
+      interactor = described_class.new(repository: spy)
       output = interactor.call(name: 'Finish this gem')
 
       expect(output).to be_success
     end
 
+    it 'creates a task' do
+      repository = spy
+      interactor = described_class.new(repository: repository)
+      task = instance_spy(Todo::Core::Entities::Task)
+      allow(Todo::Core::Entities::Task).to receive(:new).and_return(task)
+
+      interactor.call(name: 'Finish this gem')
+
+      expect(repository).to have_received(:save).with(task)
+    end
+
     it 'returns the representation of a task' do
-      interactor = described_class.new
+      interactor = described_class.new(repository: spy)
       output = interactor.call(name: 'Finish this gem')
 
       expect(output.payload[:name]).to eq 'Finish this gem'
@@ -20,14 +31,14 @@ RSpec.describe Todo::Core::Interactors::CreateTask do
 
   context 'with invalid attributes' do
     it 'returns a failed output' do
-      interactor = described_class.new
+      interactor = described_class.new(repository: spy)
       output = interactor.call(name: nil)
 
       expect(output).to be_failure
     end
 
     it 'returns an error message' do
-      interactor = described_class.new
+      interactor = described_class.new(repository: spy)
       output = interactor.call(name: nil)
 
       expect(output.payload[:error_message]).to eq 'Invalid name'
